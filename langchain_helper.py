4366bbcd11b75870f01a851d2eea96f61d502a55
Samuel O. Asare <asare11samuel@gmail.com>
2024-08-13 11:10:48 +0000

    add requirements.txt

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,43 +1,51 @@
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
-from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import OpenSearchVectorSearch
+from langchain.prompts import PromptTemplate
+from langchain_community.vectorstores.redis import Redis
+from dotenv import load_dotenv
 
 load_dotenv()
 
+embeddings = OpenAIEmbeddings()
 
-def generate_pet_name(animal_type: str, pet_color: str):
-    llm = OpenAI(temperature=0.5)
-    prompt_template_name = PromptTemplate(
-        input_variables=["animal_type", "pet_color"],
-        template="I have a {animal_type} pet and I want a cool name for it. It is {pet_color} in color. Suggest"
-                 "me five cool name for my pet"
+
+def create_vector_db_from_youtube_url(video_url) -> OpenSearchVectorSearch:
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    docs = text_splitter.split_documents(transcript)
+    # db = FAISS.from_documents(docs, embeddings)
+    # db = Redis.from_documents(docs, embeddings)
+    db = OpenSearchVectorSearch.from_documents(
+        docs, embeddings, opensearch_url="http://localhost:9200"
+    )
+    return db
+
+
+def get_response_from_query(db, query, k=4):
+    docs = db.similarity_search(query, k=k)
+    docs_page_content = " ".join([d.page_content for d in docs])
+    llm = OpenAI(model="text-embedding-ada-002")
+
+    prompt = PromptTemplate(
+        input_variables=["question", "docs"],
+        template="You are a helpful Youtube assistant that can answer questions about videos"
+                 "based on the video's transcript."
+                 "Answer the following question: {question}"
+                 "By searching the following video transcript: {docs}"
+                 "Only use factual information from the transcript to answer the question."
+                 "If you feel like you don't have enough information to answer the question, say I don't know."
+                 "Your answers should be detailed yet concise."
     )
 
-    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="pet_name")
-    response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = chain.run(question=query, docs=docs_page_content)
+    response = response.replace("\n", " ")
     return response
-
-
-def langchain_agent():
-    llm = OpenAI(temperature=0.5)
-
-    tools = load_tools(["wikipedia", "llm-math"], llm=llm)
-    agent = initialize_agent(
-        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True
-    )
-
-    result = agent.run(
-        "What is the average age of a dog? Multiply age by 3"
-    )
-
-    print(result)
-
-
-if __name__ == "__main__":
-    # print(generate_pet_name("dog", "white"))
-    langchain_agent()
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import langchain_helper as lch
-
 import streamlit as st
+import textwrap
 
-st.title("Pets name generator")
-animal_type = st.sidebar.selectbox("What is your pet type?", ("dog", "cat", "cow", "Hamster"))
+st.title("Youtube Assistant")
 
-pet_color = st.sidebar.text_area(label="What is your pet color?", max_chars=15)
+with st.sidebar:
+    youtube_url = st.sidebar.text_area(
+        label="What is your Youtube video URL?",
+        max_chars= 50,
+    )
+    with st.form(key='my_form'):
+        query = st.text_area(
+            label="Ask me about your YouTube video",
+            max_chars= 50,
+            key="query"
+        )
+        submit_btn = st.form_submit_button(label='Submit')
 
-response = lch.generate_pet_name(animal_type, pet_color)
-st.text(response['pet_name'])
+
+if query and youtube_url:
+    db = lch.create_vector_db_from_youtube_url(youtube_url)
+    response, doc = lch.get_response_from_query(db, query)
+    st.subheader("Answer")
+    st.text(textwrap.fill(response, width=80))
